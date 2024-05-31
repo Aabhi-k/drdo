9db635fc3fc6dd0dab0f2a3438a2bc9f7b4484d5
Aabhi-k <aabhigyankumar123@gmail.com>
2024-05-31 18:38:50 +0530

    updated structure

--- src/main/java/com/desidoc/management/users/super_user/SuController.java
+++ src/main/java/com/desidoc/management/users/super_user/SuController.java
@@ -3,6 +3,6 @@ package com.desidoc.management.users.super_user;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Su_user_controller {
+public class SuController {
 
 }
